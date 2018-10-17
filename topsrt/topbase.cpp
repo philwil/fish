489ac03e2f1b6af2f67ccd445d27fefaaa46966e
philwil <sysdcs@gmail.com>
2018-10-17 13:03:40 -0400

    testing

--- topsrt/topbase.cpp
+++ topsrt/topbase.cpp
@@ -1,14 +1,95 @@
 
 #include <iostream>
-#include <fstream>
 #include <iomanip>
 #include <array>
 
-//g++  -g -std=c++11 -o g topsrt.cpp
+//g++  -g -std=c++11 -o g Persons.cpp
 using namespace std;
 
-ifstream inFile;
-ofstream outFile;
+string vowels="aeiou";
+
+string testNames [5] = {
+  "anam12222",
+  "nam2",
+  "ffffaaname4",
+  "ssssnameea3",
+  "tttname2222"
+};
+
+struct Persons
+{
+  string name;
+  int nameLen;
+  int numvowels;
+};
+
+
+int NumberOfVowels(string &name)
+{
+  int i1;
+  int i2;
+  int v=0;
+
+  for (i1 = 0 ; i1 < name.size(); i1++)
+    {
+      for (i2 = 0; i2 < vowels.size(); i2++)
+	{
+	  if(name[i1] == vowels[i2]) {
+	    v++;
+	    i2 = vowels.size();
+	  }
+	}
+    }
+  return v;
+}
+
+void Input(int &max, Persons p [])
+{
+  string name;
+  cout << vowels<< " size " << vowels.size() << endl;
+  //
+  int idx = 0;
+   do
+    {
+      cout << "enter name " <<endl;
+      cin >> name;
+      cout	   << name<< " size " << name.size() << endl;
+      cout << "name "
+	   << name<< " size " << name.size()
+	   << endl;
+      p[idx].name = name;
+      p[idx].nameLen = name.size();
+      p[idx].numvowels = NumberOfVowels(name);
+      //p++;
+      idx++;
+    } while (idx < max);
+cout << " End of input" << endl<<endl;
+
+}
+
+void InputTest(int &max, Persons p [] )
+{
+  string name;
+  //cout << vowels<< " size " << vowels.size() << endl;
+  int idx = 0;
+   do
+    {
+      cout << "enter name " <<endl;
+      //cin >> name;
+      name = testNames[idx];
+      //cout	   << name<< " size " << name.size() << endl;
+      cout << "name "
+	   << name << " size " << name.size()
+	   << endl;
+      p[idx].name = name;
+      p[idx].nameLen = name.size();
+      p[idx].numvowels = NumberOfVowels(name);
+      idx++;
+      //p++;
+    } while (idx < max);
+cout << " End of input" << endl<<endl;
+
+}
 
 /* generate a random floating point number from min to max */
 double randfrom(double min, double max)
@@ -91,7 +172,7 @@ void InputTestChar(int &max,  char p [] )
       //name = testNames[idx];
       //cout	   << name<< " size " << name.size() << endl;
       cout << "random char "
-	   <<hex << (int)randomChar
+	   << randomChar
 	   << endl;
       p[idx] = randomChar;
       idx++;
@@ -101,6 +182,16 @@ cout << " End of input" << endl<<endl;
 
 }
 
+
+
+void Swap(int idx, Persons p [])
+{
+  Persons ptmp;
+  ptmp = p[idx];
+  p[idx] = p[idx+1];
+  p[idx+1] = ptmp;
+
+}
 
 
 void SwapInt(int idx, int p [])
@@ -128,6 +219,23 @@ void SwapChar(int idx, char p [])
   p[idx] = p[idx+1];
   p[idx+1] = ptmp;
 
+}
+
+void SortLength(int &max, Persons p [] )
+{
+  int swapCount;
+  do {
+    swapCount = 0;
+    for (int i = 0; i < max-1; i++)
+      {
+	if (p[i].nameLen < p[i+1].nameLen)
+	  {
+            Swap(i, p);
+	    swapCount++;
+	  }
+      }
+
+  } while (swapCount > 0);
 }
 
 
@@ -192,6 +300,48 @@ void CSort(int &max, char p [] )
 
 
 
+void SortVowels(int &max, Persons p [])
+{
+  int swapCount;
+  do {
+    swapCount = 0;
+    for (int i = 0; i < max-1; i++)
+      {
+	//cout << "SortVowels i "<< i<< " p->name ["<< p->name;
+	//cout     << "]" << " p1->name [" << (p+1)->name << "] " << endl;
+	if (
+             (p[i].numvowels < p[i+1].numvowels) ||
+	    (( p[i].numvowels == p[i+1].numvowels)
+                  && (p[i].nameLen < p[i+1].nameLen)))
+	  {
+            Swap(i, p);
+	    swapCount++;
+	  }
+      }
+
+  } while (swapCount > 0);
+
+}
+
+void OutputDisplay(int &max, Persons p [])
+{
+  int i;
+  cout<<endl;
+  cout <<setw(10)<< "  name " << "      length " <<"      vowels " << endl;
+
+  for (i = 0; i< max; i++)
+    {
+      cout << setw(15)
+            //<< " idx " << i
+	   //<< " name ["
+           << p[i].name <<" "
+	   << "    "
+           << p[i].nameLen
+	   << "        " << p[i].numvowels
+	   << endl;
+      //p++;
+    }
+}
 
 void OutputDisplayInt(int &max, int p [])
 {
@@ -244,7 +394,7 @@ void OutputDisplayChar(int &max, char p [])
       cout << setw(15)
             //<< " idx " << i
 	   //<< " name ["
-           <<hex  << (int)p[i] <<" "
+           << p[i] <<" "
 
 	   << endl;
       //p++;
@@ -261,28 +411,17 @@ int main()
     char charAry[10];
     double doubleAry[10];
     int max = 10;
-    string somestring;
-    inFile.open("indata.txt");
-    outFile.open("outdata.txt");
 
-    outFile << " Hello this is a test" << endl;
-    inFile >> somestring;
 
-    
-    InputTestInt(max, intAry);
-    ISort(max,intAry);
+  InputTestInt(max, intAry);
+  ISort(max,intAry);
 
-    OutputDisplayInt(max, intAry);
-    InputTestChar(max, charAry);
-    CSort(max,charAry);
-    OutputDisplayChar(max, charAry);
-    InputTestDouble(max, doubleAry);
-    DSort(max,doubleAry);
-    OutputDisplayDouble(max, doubleAry);
+  OutputDisplayInt(max, intAry);
+  InputTestChar(max, charAry);
+  CSort(max,charAry);
+  OutputDisplayChar(max, charAry);
+  InputTestDouble(max, doubleAry);
+  DSort(max,doubleAry);
+  OutputDisplayDouble(max, doubleAry);
 
-    cout << " infile read [" << somestring<<"]"<<endl;
-    inFile.close();
-    outFile.close();
-    
   return 0;
-}
